fca98a80f517f93676d620976566929d40207fee
Amarjeet5822 <amar.bst5822@gmail.com>
2024-07-22 12:57:31 +0530

    In view implemented a user registration function

--- First/BLOG/BlogApp/views.py
+++ First/BLOG/BlogApp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from BlogApp.models import Profile,Post
+from django.contrib.auth.models import User
+from .serializers import PostSerializer,UserSerializer,ProfileSerializer
+
+from rest_framework.decorators import APIView
+from rest_framework.response import Response 
+from rest_framework import status
+
 
 # Create your views here.
+class RegistrationView(APIView):
+    # authentication_classes = [AllowAny]
+
+    def post(self,request):
+        serializer = UserSerializer(data=request.data)
+        print("before checking",serializer)
+        if serializer.is_valid():
+            print("before checking",serializer)
+            user = serializer.save()
+            profile = Profile(user=user,user_type=request.data.get('user_type'))
+            profile.save()
+            return Response({'message':'Signup Successfull'},status=status.HTTP_201_CREATED)
+        return Response({'message':'Username or Password is invalid. Enter correctly'},status=status.HTTP_400_BAD_REQUEST)
+    
